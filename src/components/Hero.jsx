1f08cdf72c14af866a99ba35dd6ea6a39bd84a6a
Skye <scottngocbui@gmail.com>
2023-10-03 18:29:17 -0400

    Started working on NavBar and Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Hero = () => {
   return (
     <section className='grid grid-cols-4 mx-[20px] gap-[20px] md:grid-cols-6 lg:grid-cols-12 place-items-center h-screen'>
-        <div className='text-5xl col-span-4 md:col-span-6 lg:col-span-12'>Hello</div>
+        <div className='grid text-5xl col-span-4 md:col-span-6 lg:col-span-12'>Hi, My name is</div>
+        <div className='grid text-5xl col-span-4 md:col-span-6 lg:col-span-12'>Scott Bui</div>
     </section>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,16 @@ const Navbar = () => {
   return (
     <section className='grid grid-cols-4 mx-[20px] gap-[20px] h-20 md:grid-cols-6 lg:grid-cols-12'>
         <div className='flex flex-row items-center justify-between col-span-4 md:col-span-6 lg:col-span-12 px-8'>
-            <p>ScottBui.Dev</p>
+            <div className='text-xl border-2 px-8 py-4 rounded-[16px]'>
+              ScottBui.Dev
+            </div>
+            <ul className='hidden text-xl md:flex md:flex-row'>
+              <li className='px-8 py-4'>Hero</li>
+              <li className='px-8 py-4'>About Me</li>
+              <li className='px-8 py-4'>Skills</li>
+              <li className='px-8 py-4'>Projects</li>
+              <li className='px-8 py-4'>Contact Me</li>
+            </ul>
         </div>
     </section>
   )
