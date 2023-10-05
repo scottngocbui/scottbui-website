4d618ce217e9e7f107f99a57334d86427e916754
Skye <scottngocbui@gmail.com>
2023-10-05 01:48:22 -0400

    Changed Buttons hover effects

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,16 @@ import Me from './assets/me1.png'
 const App = () => {
   return (
     <div className='bg-mamba bg-dgray min-h-screen text-pblue font-poppins'>
-      <div className='grid grid-cols-4 mx-[20px] md:grid-cols-8 lg:grid-cols-12 gap-[20px] md:mx-[70px]'>
+      <div className='grid grid-cols-4 mx-[20px] md:grid-cols-8 lg:grid-cols-12 gap-[20px] md:mx-[70px] pt-20'>
         <div className='bg-rblack col-span-4 h-[775px] rounded-[16px] mt-20'>
           <div className='flex flex-col items-center'>
             <img src={Me} alt='Me' className='w-[200px] -mt-[50px] bg-gray rounded-lg'/>
             <div className='text-5xl my-2'>Scott Bui</div>
             <div className='text-2xl bg-gray my-2 p-2 rounded-lg text-white'>Front-End Developer</div>
             <div className='flex flex-row my-2'>
-              <div className='mx-2 p-2 bg-gray rounded-lg'><BiLogoLinkedin size='20'/></div>
-              <div className='mx-2 p-2 bg-gray rounded-lg'><BsGithub size='20'/></div>
-              <div className='mx-2 p-2 bg-gray rounded-lg'><FaFigma size='20'/></div>
+              <div className='mx-2 p-2 bg-gray rounded-lg cursor-pointer hover:bg-blue hover:text-gray transition ease'><BiLogoLinkedin size='20'/></div>
+              <div className='mx-2 p-2 bg-gray rounded-lg cursor-pointer hover:bg-blue hover:text-gray transition ease'><BsGithub size='20'/></div>
+              <div className='mx-2 p-2 bg-gray rounded-lg cursor-pointer hover:bg-blue hover:text-gray transition ease'><FaFigma size='20'/></div>
               <div></div>
             </div>
             <div className='grid grid-cols-1 divide-y border-2 py-2 px-4 lg:px-8 rounded-lg my-2'>
@@ -54,7 +54,7 @@ const App = () => {
               </div>
             </div>
             <div className='mt-2'>
-              <a target='_blank' rel='noopener noreferrer' className="reg-text relative flex shrink items-center justify-center p-4 px-6 py-3 overflow-hidden transition duration-300 ease-out border-2 rounded-[16px] group">
+              <a target='_blank' rel='noopener noreferrer' className="cursor-pointer reg-text relative flex shrink items-center justify-center p-4 px-6 py-3 overflow-hidden transition duration-300 ease-out border-2 rounded-[16px] group">
                   <span className="absolute inset-0 flex items-center justify-center w-full h-full duration-300 -translate-x-full group-hover:translate-x-0 ease">
                       <BsDownload size={24}/>
                   </span>
@@ -66,15 +66,15 @@ const App = () => {
         </div>
         <div className='flex bg-rblack col-span-4 md:col-end-9 lg:col-end-13 lg:col-span-5 h-[150px] rounded-[16px] md:mt-20 justify-center items-center'>
           <div className='flex flex-row justify-center'>
-            <div className='bg-blue flex flex-col items-center justify-center w-[100px] py-2 mx-2 border-2 rounded-lg'>
+            <div className='bg-blue text-gray flex flex-col items-center justify-center w-[100px] py-2 mx-2 border-2 rounded-lg'>
               <span className=''><BsPersonVcard size="40"/></span>
               <span className="relative">About Me</span>
             </div>
-            <div className='flex flex-col items-center justify-center w-[100px] py-2 mx-2 border-2 rounded-lg'>
+            <div className='flex flex-col items-center justify-center w-[100px] py-2 mx-2 border-2 rounded-lg cursor-pointer hover:bg-blue hover:text-gray transition ease'>
               <span className=''><BsFolder size="40"/></span>
-              <span className="relative">Projects</span>
+              <span className="relative">Experience</span>
             </div>
-            <div className='flex flex-col items-center justify-center w-[100px] py-2 mx-2 border-2 rounded-lg'>
+            <div className='flex flex-col items-center justify-center w-[100px] py-2 mx-2 border-2 rounded-lg cursor-pointer hover:bg-blue hover:text-gray transition ease'>
               <span className=''><BsChatLeftText size="40"/></span>
               <span className="relative">Contact Me</span>
             </div>
